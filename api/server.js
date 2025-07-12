823440002e2bb90cfb5d8a9e94056695881a0302
Foad Ebrahimi <138124428+foadEbrahimi@users.noreply.github.com>
2025-07-12 16:30:49 +0330

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add delay middleware
 server.use((req, res, next) => {
-  setTimeout(next, 2000); // 1000ms = 1 second delay
+  setTimeout(next, 1000); // 1000ms = 1 second delay
 });
 // Add this before server.use(router)
 server.use(
