297ba9b9a7c5a1fdca8048563443978dfb22d6d7
Foad <fwad40901@gmail.com>
2025-07-12 14:19:22 +0330

    add delay to response

--- api/server.js
+++ api/server.js
@@ -1,19 +1,25 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
+// Add delay middleware
+server.use((req, res, next) => {
+  setTimeout(next, 2000); // 1000ms = 1 second delay
+});
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(
+  jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+    '/blog/:resource/:id/show': '/:resource/:id',
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
